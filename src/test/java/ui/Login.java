4bdbdf63d275f8d08a69bdf6f95d2cc179067c25
Prathyusha <Prathyubavanam@gmail.com>
2023-10-26 13:49:25 -0400

    commit

---
+++ src/test/java/ui/Login.java
@@ -0,0 +1,43 @@
+package ui;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Login {
+
+	@BeforeTest
+	public void loginToApplication() {
+		System.out.println("Login to application");
+	}
+	@AfterTest
+	public void logoutToApplication() {
+		System.out.println("Logout to application");
+	}
+	
+	@BeforeMethod
+	public void connectToDB() {
+		System.out.println("DB connected");
+	}
+	
+	@AfterMethod
+	public void disconnectToDB() {
+		System.out.println("DB dis-connected");
+	}
+	@Test(priority=1,description="This is Login test")
+	public void loginTest() {
+		System.out.println("Login is sucessfull");
+
+	}
+
+	@Test(priority=2,description="This is Logout test")
+	public void logoutTest() {
+		System.out.println("Logout is sucessfull");
+
+	}
+	
+	
+	//Testing for Jenkins
+}
